a3036c8d53720922429ed4c6e9673f56bb530db4
cutelee <soyoung.dev@gmail.com>
2019-09-19 13:10:46 +0900

    포스트 프로필 정보 수정

--- src/components/Post/Author/Author.js
+++ src/components/Post/Author/Author.js
@@ -1,24 +1,18 @@
 // @flow strict
 import React from 'react';
-import { getContactHref } from '../../../utils';
 import styles from './Author.module.scss';
 import { useSiteMetadata } from '../../../hooks';
 
 const Author = () => {
   const { author } = useSiteMetadata();
-  console.log('author info:', author);
   return (
     <div className={styles['author']}>
+      <a href={`https://github.com/${author.contacts.github}`}>
+        <img className={styles['author__image']} alt="profile images" src={author.photo} />
+      </a>
       <p className={styles['author__bio']}>
+        <span className={styles['author__bio-name']}>{author.name}</span>
         {author.bio}
-        <a
-          className={styles['author__bio-twitter']}
-          href={getContactHref('twitter', author.contacts.twitter)}
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <strong>{author.name}</strong> on Twitter
-        </a>
       </p>
     </div>
   );
