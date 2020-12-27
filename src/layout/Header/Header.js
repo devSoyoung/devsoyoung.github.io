057a1497b6e27537f565c4f366c7aa2cb1324a7c
SoYoung Lee <soyoung.dev@gmail.com>
2020-12-27 16:09:40 +0900

    GNB - TIL 제거

--- src/layout/Header/Header.js
+++ src/layout/Header/Header.js
@@ -18,36 +18,25 @@ const Header = ({ siteTitle, siteDescription, type }) => {
                     </Link>
                     <Link
                         className={"header-menu-item".concat(
-                            type === "til" ? " selected" : ""
-                        )}
-                        to="/til"
-                    >
-                        TIL
-                    </Link>
-                    <Link
-                        className={"header-menu-item".concat(
                             type === "category" ? " selected" : ""
                         )}
                         to="/category"
                     >
                         Category
                     </Link>
-                    {/*<Link className={"header-menu-item".concat(type === "about" ? " selected" : "")} to="/about">*/}
-                    {/*  About*/}
-                    {/*</Link>*/}
                     <a
                         className="header-menu-item link github"
                         href="https://github.com/devSoyoung"
                         target="_blank"
                     >
-                        <i className="fab fa-github-alt"></i>
+                        <i className="fab fa-github-alt"/>
                     </a>
                     <a
                         className="header-menu-item link"
                         href="https://www.linkedin.com/in/cutelee/"
                         target="_blank"
                     >
-                        <i className="fab fa-linkedin-in"></i>
+                        <i className="fab fa-linkedin-in"/>
                     </a>
                 </div>
                 <h1 className="header-title">{siteTitle}</h1>
