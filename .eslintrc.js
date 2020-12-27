e55b62b61df500a3bf0cda3b5370cc686e73be65
SoYoung Lee <soyoung.dev@gmail.com>
2020-12-27 16:04:11 +0900

    eslint, prettier 설정

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    extends: ["react-app"],
+    rules: {
+        semi: ["error", "always"],
+        quotes: ["error", "double"],
+        "comma-dangle": ["error", "never"],
+        indent: ["error", 4],
+        "quote-props": ["error", "consistent-as-needed"]
+    }
+};
